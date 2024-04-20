656eda96806b07e51e3708e31dbaf275de57b582
steady-on <roen.steady.on@gmail.com>
2024-04-20 18:29:45 +0900

    refactor(WordTestServiceImpl): 접근제어 적용 및 주석 정리
    - 사용하지 않는 메서드 주석처리
    - 그 외 띄어쓰기 및 줄바꿈 정리

--- GGomVoca/GGomVoca/Service/WordTest/WordTestServiceImpl.swift
+++ GGomVoca/GGomVoca/Service/WordTest/WordTestServiceImpl.swift
@@ -8,35 +8,34 @@
 import Foundation
 import Combine
 
-class WordTestServiceImpl: WordTestService {
-    
+final class WordTestServiceImpl: WordTestService {
     //Repository
-    var coreDataRepo : CoreDataRepository
-    var cloudDataRepo : CloudKitRepository
+    private let coreDataRepo: CoreDataRepository
+    private let cloudDataRepo: CloudKitRepository
     
-    init(coreDataRepo : CoreDataRepository, cloudDataRepo : CloudKitRepository ){
+    init(coreDataRepo: CoreDataRepository, cloudDataRepo: CloudKitRepository) {
         self.coreDataRepo = coreDataRepo
         self.cloudDataRepo = cloudDataRepo
     }
     
-    //디스크에 저장.
+    /// 디스크에 저장.
     func saveContext() {
         coreDataRepo.saveContext()
     }
     
-    // MARK: 일치하는 id의 단어장 불러오기
+    /// 일치하는 id의 단어장 불러오기
     func getVocabularyFromId(vocabularyID: Vocabulary.ID) -> AnyPublisher<Vocabulary, RepositoryError> {
         return coreDataRepo.getVocabularyFromID(vocabularyID: vocabularyID!)
     }
     
-    // MARK: 일치하는 id의 단어장 단어리스트 불러오기
-    //TODO: Cloud Fetch
+    /// 일치하는 id의 단어장 단어리스트 불러오기
+    // TODO: Cloud Fetch
     func fetchWordList(vocabulary: Vocabulary) -> AnyPublisher<[Word], RepositoryError> {
         return coreDataRepo.getWordListFromVoca(voca: vocabulary)
     }
     
-    // MARK: - Core Data에 시험결과 저장
-    func testResult() {
-       
-    }
+    /// - Core Data에 시험결과 저장
+//    func testResult() {
+//       
+//    }
 }
